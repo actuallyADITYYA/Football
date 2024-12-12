bdfb1365f5f2d362e1d04a44569bff989ce8eb11
CaptainJack2491 <jayrupnakawala@gmail.com>
2024-12-12 17:21:20 +0000

    containerized the application

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 });
 
 // Enable CORS
-app.use(cors());
+// app.use(cors());
 
 // Alternatively, specify allowed origins
 app.use(
@@ -188,7 +188,9 @@ app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   })
 
-mongoose.connect('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.3', {
+const MONGO_URI = 'mongodb://mongo:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.3'
+
+mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
